85c2527d9478485d42b3a90fb02635887623487e
Moshele Seoka <seokamk@yahoo.com>
2025-04-21 17:24:17 +0200

    Refactor ServiceCollectionExtensions to improve code organization and add XML documentation for ConfigureMappingAndValidators method

--- SDA.Common.Configuration/Extensions/ServiceCollectionExtensions.cs
+++ SDA.Common.Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,16 @@
-﻿using FluentValidation;
+﻿using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
 
 namespace SDA.Common.Configuration.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Configures the service collection by adding AutoMapper and Fluent Validation from the executing assembly.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to configure.</param>
+        /// <returns>The configured <see cref="IServiceCollection"/> instance.</returns>
         public static IServiceCollection ConfigureMappingAndValidators(this IServiceCollection services)
         {
             return services
