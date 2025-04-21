fe54dc96d03fff93dc6b6f4a649e2c6621cf81ab
Moshele Seoka <seokamk@yahoo.com>
2025-04-21 15:57:15 +0200

    Add global using directives for common namespaces in the project

---
+++ SDA.Common/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System.ComponentModel;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using SDA.Common.Contracts;
+global using SDA.Common.Models;
---
+++ SDA.Common/Models/Response/Response.cs
@@ -0,0 +1,20 @@
+﻿namespace SDA.Common.Models.Response
+{
+    /// <summary>
+    /// Represents a generic response structure for API operations.
+    /// </summary>
+    /// <typeparam name="T">The type of the data returned in the response.</typeparam>
+    /// <remarks>
+    /// This class implements the <see cref="IResponse"/> interface and includes
+    /// properties for the response data, a message, and a success indicator.
+    /// </remarks>
+    public class Response<T> : IResponse
+        where T : class
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+
+        [DefaultValue(true)]
+        public bool Succeeded { get; set; }
+    }
+}
