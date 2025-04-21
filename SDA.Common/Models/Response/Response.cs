50dd337972b9fcd17a67619de97e3db01f64ed50
Moshele Seoka <seokamk@yahoo.com>
2025-04-21 16:05:23 +0200

    Add .gitignore file and update Response class documentation

--- SDA.Common/Models/Response/Response.cs
+++ SDA.Common/Models/Response/Response.cs
@@ -1,20 +1,23 @@
 ﻿namespace SDA.Common.Models.Response
 {
-    /// <summary>
-    /// Represents a generic response structure for API operations.
-    /// </summary>
-    /// <typeparam name="T">The type of the data returned in the response.</typeparam>
-    /// <remarks>
-    /// This class implements the <see cref="IResponse"/> interface and includes
-    /// properties for the response data, a message, and a success indicator.
-    /// </remarks>
     public class Response<T> : IResponse
         where T : class
     {
+        /// <property name="Data">
+        /// Gets or sets the data of type <typeparamref name="T"/> associated with the response.
+        /// </property>
+        /// <typeparam name="T">The type of the data contained in the response.</typeparam>
         public T Data { get; set; }
+
+        /// <property name="Message">
+        /// Gets or sets the message associated with the response.
+        /// </property>
         public string Message { get; set; }
 
         [DefaultValue(true)]
+        /// <property name="Succeeded">
+        /// Indicates whether the response was successful. Default is true.
+        /// </property>
         public bool Succeeded { get; set; }
     }
 }
