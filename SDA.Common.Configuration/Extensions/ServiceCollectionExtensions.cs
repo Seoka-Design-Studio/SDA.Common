85211e6aec47b5b6039f1bb833e940bee44faa70
Moshele Seoka <seokamk@yahoo.com>
2025-04-21 17:22:42 +0200

    Add AutoMapper and FluentValidation configuration; create ServiceCollectionExtensions and GlobalMapping classes

---
+++ SDA.Common.Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace SDA.Common.Configuration.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureMappingAndValidators(this IServiceCollection services)
+        {
+            return services
+                .AddAutoMapper(Assembly.GetExecutingAssembly())
+                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
--- SDA.Common.Configuration/GlobalUsings.cs
+++ SDA.Common.Configuration/GlobalUsings.cs
@@ -1,1 +1,1 @@
-﻿
+﻿global using SDA.Common.Core.Models;
